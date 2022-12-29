670511ecc698fc648101aa5faa63f78f896e0382
Adarsh P Nair <adarshpn122@gmail.com>
2022-12-29 21:35:50 +0530

    added the bitset

--- 6_bitwise_operations/main.cpp
+++ 6_bitwise_operations/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+//library used to convert to binary
+#include <bitset>
 
 int main()
 {
     /* code */
+     std::cout << std::endl
+              << "Printing integer to binary (bitset) ----------------------------------------------------------------------------------------------------------> " << std::endl;
+
+    short int int_var = 23;
+    unsigned short bin_var = 0b1001100100;
+    
+    //here 16 is the size of the bits
+    std::cout << std::bitset<16>(int_var) << std::endl;
+    std::cout << std::bitset<16>(bin_var) << std::endl;
+
+    
+    short int right_shift_value = bin_var << 2;
+    std::cout << std::bitset<16>(right_shift_value) << std::endl;
+
+    std::cout << sizeof(bin_var) << std::endl;
+    std::cout << sizeof(int_var) << std::endl;
+
     return 0;
 }
